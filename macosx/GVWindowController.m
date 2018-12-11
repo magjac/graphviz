c170fab4197952b8cbeae5b4498ff13763da9b55
Brendan Shanks <brendan@bslabs.net>
2018-12-11 12:51:29 -0800

    macOS: Always call [super dealloc] in GVWindowController dealloc
    Apparently added because of crashes on 10.7 and up, I haven't seen any
    on 10.13. Crashing here indicates a problem elsewhere anyway.

--- macosx/GVWindowController.m
+++ macosx/GVWindowController.m
@@ -116,14 +116,8 @@
 }
 - (void)dealloc
 {
-    char darwinStr[256];
-    size_t size = sizeof(darwinStr);
     [[NSNotificationCenter defaultCenter] removeObserver:self name:@"GVGraphDocumentDidChange" object:[self document]];
-    sysctlbyname("kern.osrelease", darwinStr, &size, NULL, 0);
-    NSString *darwinVer = [NSString stringWithCString:darwinStr encoding:NSASCIIStringEncoding]; 
-    NSString *baseVer = @"11";
-    if ([darwinVer compare:baseVer] < 0)
-         [super dealloc];
+    [super dealloc];
 }
 
 
