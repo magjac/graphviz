6d6ed664b1982fb793acab2c9558c9c8e688c5f8
Matthew Fernandez <matthew.fernandez@gmail.com>
2020-07-05 15:14:27 -0700

    remove unused vmsegment API

--- lib/vmalloc/vmalloc.h
+++ lib/vmalloc/vmalloc.h
@@ -126,7 +126,6 @@ extern "C" {
     extern long vmsize(Vmalloc_t *, void *);
 
     extern Vmalloc_t *vmregion(void *);
-    extern void *vmsegment(Vmalloc_t *, void *);
     extern int vmset(Vmalloc_t *, int, int);
 
     extern void *vmdbwatch(void *);
